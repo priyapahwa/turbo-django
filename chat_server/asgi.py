1f4496bcfc4d3b21dfba91fc474f9c6c67495ddc
priyapahwa <pahwa.priya19@gmail.com>
2022-03-06 20:22:37 +0530

    Modify `asgi.py` to listen WebSocket

--- chat_server/asgi.py
+++ chat_server/asgi.py
@@ -10,7 +10,13 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter
+from turbo.consumers import TurboStreamsConsumer
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat_server.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": TurboStreamsConsumer.as_asgi()
+})
+
